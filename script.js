757410a8cd95e86bfa45b8fee509ee6dc0502bed
marcosjavier1997 <33172949+marcosjavier1997@users.noreply.github.com>
2023-09-29 07:39:40 -0300

    Simulador

--- script.js
+++ script.js
@@ -18,14 +18,179 @@ var r=0
 
  const simulacion = document.getElementById("simulacion");
  simulacion.addEventListener("click", simulate);
+
+ const normal = document.getElementById("normal");
+ normal.addEventListener("click", vernormal);
+ const uniforme = document.getElementById("uniforme");
+ uniforme.addEventListener("click", veruniforme);
+ const geometrica = document.getElementById("geometrica");
+ geometrica.addEventListener("click", vergeometrica);
  
+ function vernormal() {
+    document.getElementById("problema").hidden = true
+    document.getElementById("simulador-uniforme").hidden = true
+    document.getElementById("simulador-geometrica").hidden = true
+    document.getElementById("simulador-normal").hidden = false
+    document.getElementById("simulador-uniforme-response").hidden = true
+    document.getElementById("simulador-geometrica-response").hidden = true
+}
+
+const simularN = document.getElementById("simular-normal");
+simularN.addEventListener("click", simularnormal);
+
+function simularnormal() {
+    array = []
+    itamaraca()
+    var media = Number(document.getElementById("media-input").value)
+    var desv = Number(document.getElementById("desv-input").value)
+    var tiempo = Number(document.getElementById("tiempo-input").value)
+
+    var x=0;
+    for(var i=1; i<=tiempo;i++){
+        x = x+distribucionNormal(media,desv)
+    }
+    var random = 0;
+    var devueltas = 0;
+    for(i=1;i<=x;i++){
+        random = numeroRandom()
+        if(random<=0.01){
+            devueltas+=20;
+        }
+        else {
+            if(random<=0.03){
+                devueltas+=10;
+            }
+            else {
+                if(random<=0.06){
+                    devueltas+=6;
+                }
+                else {
+                    if(random<=0.11){
+                        devueltas+=4;
+                    }
+                    else {
+                        if(random<=0.19){
+                            devueltas+=2;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    var neto = x - devueltas
+
+    document.getElementById("simulador-normal-response").hidden = false
+
+    const ingresadas = document.getElementById("fichas-ingresadas");
+    ingresadas.innerHTML=x.toFixed(2)
+    const develtas = document.getElementById("fichas-devueltas");
+    develtas.innerHTML=devueltas.toFixed(2)
+    const recaudadas = document.getElementById("fichas-recaudadas");
+    recaudadas.innerHTML=neto.toFixed(2)
+
+    document.getElementById("media").innerHTML = media
+    document.getElementById("desviacion").innerHTML = desv
+    document.getElementById("tiempo1").innerHTML = tiempo
+    document.getElementById("tiempo2").innerHTML = tiempo
+    document.getElementById("tiempo3").innerHTML = tiempo
+
+}
+
+
+function distribucionNormal(media,desviac){
+    var sum=0;
+    for(var i=1;i<=12;i++){
+        sum=sum+numeroRandom()
+    }
+    var x = desviac*(sum-6)+media
+    return x;
+}
+ function veruniforme() {
+    document.getElementById("problema").hidden = true
+    document.getElementById("simulador-normal").hidden = true
+    document.getElementById("simulador-geometrica").hidden = true
+    document.getElementById("simulador-uniforme").hidden = false
+    document.getElementById("simulador-normal-response").hidden = true
+    document.getElementById("simulador-geometrica-response").hidden = true
+}
+
+const simularU = document.getElementById("simular-uniforme");
+simularU.addEventListener("click", simularuniforme);
+
+function simularuniforme(){
+    array = []
+    itamaraca()
+    var a  = Number(document.getElementById("a-input").value)
+    var b = Number(document.getElementById("b-input").value)
+    var jugadores = Number(document.getElementById("jugadores-input").value)
+
+    var x=0;
+    for(var i=1; i<=jugadores;i++){
+        x = x+distribucionuniforme(a,b)
+    }
+
+    document.getElementById("simulador-uniforme-response").hidden = false
+
+    const usoMaquina = document.getElementById("uso-maquina");
+    usoMaquina.innerHTML=x.toFixed(2)
+    document.getElementById("a").innerHTML = a
+    document.getElementById("b").innerHTML = b
+    document.getElementById("jugadores").innerHTML = jugadores
+}
+
+function distribucionuniforme(a,b){
+    var x = a+(b-a)*numeroRandom()
+    return x;
+}
+
+ function vergeometrica() {
+    document.getElementById("problema").hidden = true
+    document.getElementById("simulador-normal").hidden = true
+    document.getElementById("simulador-uniforme").hidden = true
+    document.getElementById("simulador-geometrica").hidden = false
+    document.getElementById("simulador-normal-response").hidden = true
+    document.getElementById("simulador-uniforme-response").hidden = true
+}
+
+const simularG = document.getElementById("simular-geometrica");
+simularG.addEventListener("click", simulargeometrica);
+
+function simulargeometrica(){
+    array = []
+    itamaraca()
+    var p  = Number(document.getElementById("p-input").value)
+
+    var x = distribuciongeometrica(p/100)
+
+    document.getElementById("simulador-geometrica-response").hidden = false
+
+    const fichasN = document.getElementById("fichas-necesarias");
+    fichasN.innerHTML=x
+    document.getElementById("p").innerHTML = p
+}
+
+function distribuciongeometrica(p){
+    var x = 0
+    while(numeroRandom()>p){
+        x=x+1
+    }
+    return x;
+}
+
  function vertragamonedas() {
     document.getElementById("contenedorslot").hidden = false
     document.getElementById("contenedorsimu").hidden = true
 }
  function versimulador() {
+    document.getElementById("problema").hidden = false
+    document.getElementById("simulador-normal").hidden = true
+    document.getElementById("simulador-geometrica").hidden = true
+    document.getElementById("simulador-uniforme").hidden = true
     document.getElementById("contenedorslot").hidden = true
     document.getElementById("contenedorsimu").hidden = false
+    document.getElementById("simulador-normal-response").hidden = true
+    document.getElementById("simulador-uniforme-response").hidden = true
+    document.getElementById("simulador-geometrica-response").hidden = true
 }
 
 palanca.addEventListener("click", giro);
@@ -36,11 +201,15 @@ function obtenerAleatorios() {
     return rutaImagen;
 }
 
-function obtenerValores() {
-    //var random = Math.random();
-    var random = array[r];
+function numeroRandom(){
     r++;
     if(r==10000){r=0}
+    return array[r]
+}
+function obtenerValores() {
+    //var random = Math.random();
+    var random = numeroRandom()
+
     fichasPerdidas++;
     fichas--;
     if(random<=0.01){
